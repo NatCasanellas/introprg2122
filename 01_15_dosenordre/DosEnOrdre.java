67788ea349c97063e673e885f696f7cb698ca857
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-10-19 18:49:31 +0200

    Exercici començat

--- 01_15_dosenordre/DosEnOrdre.java
+++ 01_15_dosenordre/DosEnOrdre.java
@@ -4,10 +4,11 @@
 public class DosEnOrdre {
     public static void main(String[] args) {
         System.out.println("Primer?");
-        int primer = Integer.parseInt(Entrada.readLine(1));
+        int primer = Integer.parseInt(Entrada.readLine());
 
         if (primer < segon) {
             System.out.println("El primer es mes petit que el segon");
+                
         } else {
             System.out.println (primer + " i " + segon);
     }
