e2c046d83d0250a43697f58d7333ea033fabb6b3
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-10-26 23:06:04 +0200

    Exercici finalitzat

---
+++ 01_20_mesosanys/MesosAnys.java
@@ -0,0 +1,23 @@
+/*
+ * Programa que ordena dos nombres
+ */
+public class MesosAnys {
+    public static void main(String[] args) {
+        System.out.println("Mes?");
+        int mes = Integer.parseInt(Entrada.readLine());
+                
+        System.out.println("Any?");
+        int any = Integer.parseInt(Entrada.readLine());
+        
+        
+        if (mes = (-mes) {
+            System.out.println(mes + " i " + any);
+                
+        } else {
+            System.out.println("Anterior " + mes + "/" + any + " i posterior " + mes + "/" + any);
+            
+        
+            
+    }
+  }
+}
---
+++ 01_21_tresenordre/TresEnOrdre.java
@@ -0,0 +1,29 @@
+/*
+ * Programa que ordena dos nombres
+ */
+public class TresEnOrdre {
+    public static void main(String[] args) {
+        System.out.println("Primer?");
+        int primer = Integer.parseInt(Entrada.readLine());
+        
+        System.out.println("Segon?");
+        int segon = Integer.parseInt(Entrada.readLine());
+        
+        System.out.println("Tercer?");
+        int tercer = Integer.parseInt(Entrada.readLine());
+        
+        
+        if (primer < segon) {
+            System.out.println(primer + " i " + segon);
+            
+        if (segon < tercer) {
+            System.out.println(primer + " i " + segon);    
+                
+        } else {
+            System.out.println(segon + " , " + primer + " i" + tercer);
+            
+        
+      }      
+    }
+  }
+}
