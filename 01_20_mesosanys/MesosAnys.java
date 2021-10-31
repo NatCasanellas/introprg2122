bcbc181f530d261b947e7af870b8c8406f2ea2c2
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-10-31 18:00:39 +0100

    Exercici Finalitzat

--- 01_20_mesosanys/MesosAnys.java
+++ 01_20_mesosanys/MesosAnys.java
@@ -4,20 +4,19 @@
 public class MesosAnys {
     public static void main(String[] args) {
         System.out.println("Mes?");
-        int mes = Integer.parseInt(Entrada.readLine());
+        int numero = Integer.parseInt(Entrada.readLine());
                 
         System.out.println("Any?");
         int any = Integer.parseInt(Entrada.readLine());
         
         
-        if (mes = ) {
-            System.out.println(mes + " i " + any);
-                
+        if (numero == 1 ) {
+              System.out.println("Anterior 12/" + (any -1) + " i posterior 2/" +(any));                
+        } else if (numero == 12) {
+            System.out.println("Anterior 11/" + (any) + " i posterior 1/" + (any + 1));
+            
         } else {
-            System.out.println("Anterior " + mes + "/" + any + " i posterior " + mes + "/" + any);
-            
-        
-            
+            System.out.println("Anterior " + (numero -1) + "/" + any +" "+ " i posterior " +(numero +1)+"/"+any);
     }
   }
 }
