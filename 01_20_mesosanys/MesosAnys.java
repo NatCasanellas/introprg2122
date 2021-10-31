47c81f04555a7a2227598943a19ae7a689a6da5c
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-10-31 18:06:18 +0100

    Exercici Finalitzat

--- 01_20_mesosanys/MesosAnys.java
+++ 01_20_mesosanys/MesosAnys.java
@@ -11,12 +11,12 @@ public class MesosAnys {
         
         
         if (numero == 1 ) {
-              System.out.println("Anterior 12/" + (any -1) + " i posterior 2/" +(any));                
+              System.out.println("Anterior 12/" + (any -1) + "i posterior 2/" +(any));                
         } else if (numero == 12) {
-            System.out.println("Anterior 11/" + (any) + " i posterior 1/" + (any + 1));
+            System.out.println("Anterior 11/" + (any) + "i posterior 1/" + (any + 1));
             
         } else {
-            System.out.println("Anterior " + (numero -1) + "/" + any +" "+ " i posterior " +(numero +1)+"/"+any);
+            System.out.println("Anterior " + (numero -1) + "/" + any +" "+ "i posterior " +(numero +1)+"/"+any);
     }
   }
 }
