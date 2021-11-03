7a1f616553845e1c3eaf04c058d3e8d34260fec6
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-03 17:25:15 +0100

    Exercici Finalitzat

--- 01_29_sumanotes/SumaNotes.java
+++ 01_29_sumanotes/SumaNotes.java
@@ -21,7 +21,7 @@ public class SumaNotes {
             valor = Integer.parseInt(Entrada.readLine());
         }
 
-        while (valor < 100) {
+        while (valor >= 100) {
             suma = suma + valor;
             System.out.println("Introdueix una nota");
             valor = Integer.parseInt(Entrada.readLine());
