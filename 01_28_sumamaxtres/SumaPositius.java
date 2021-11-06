d4d18a2f0fe45c94bf541e09000526e6696bc95f
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-06 22:26:57 +0100

    Exercici Finalitzat

--- 01_28_sumamaxtres/SumaPositius.java
+++ 01_28_sumamaxtres/SumaPositius.java
@@ -1,9 +1,12 @@
 /*
-El programa 
+* El programa amplia la versio del progrmama SumaPositius, perque en comptes de
+* demanar  tres valors sempre, ara només en demani fins el primer cop que 
+* s'introdueixi un valor negatiu. Encara cal sumar només els positius.
 */
 
 public class SumaPositius {
     public static void main(String[] args) {
+    
 
         // creem la variable on guardarem el resultat de sumar
         int suma = 0;   // inicialment no hem sumat res i per tant és 0
@@ -15,9 +18,7 @@ public class SumaPositius {
         // processem el primer valor
         System.out.println("Introdueix el primer valor");
         valor = Integer.parseInt(Entrada.readLine());
-        if (valor >= 0)
-          if (valor != 0) {
-        
+        if (valor >= 0) {
             suma = suma + valor;
         }
 
@@ -33,10 +34,11 @@ public class SumaPositius {
            valor = Integer.parseInt(Entrada.readLine());
            if (valor <= 0) {
                suma = suma + valor;
-       
-             
+            
          
         // mostrem el resultat
+        
+        } else {
         System.out.println("La suma és " + suma);
     }
   }
