9b96374f341a7314821f307672262f8416f2d48e
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-06 18:12:56 +0100

    Exercici Finalitzat

--- 01_26_ascensor/Ascensor.java
+++ 01_26_ascensor/Ascensor.java
@@ -1,6 +1,6 @@
 /*
- * Aquest programa 
- * 
+ * Aquest programa es un ascensor d'un edifici amb planta baixa i dos pisos 
+ * (primer pis i segon pis)
  */
 public class Ascensor {
     public static void main(String[] args) {
@@ -11,20 +11,26 @@ public class Ascensor {
             String plantaNova = Entrada.readLine();
         
         
-      /* 
-      Planta baixa
-      */
+      // Planta baixa 
+      
+      
+      
+      
         if (plantaActual.equals("planta baixa") && plantaNova.equals ("pujar un")) {
-            System.out.println("primer pis");
+             System.out.println("primer pis");
             
         } else if (plantaActual.equals("planta baixa") && plantaNova.equals ("pujar dos")) {
             System.out.println("segon pis");
+       
+       // Primer pis
             
         } else if (plantaActual.equals("primer pis") && plantaNova.equals ("pujar un")) {
             System.out.println("segon pis");
             
         } else if (plantaActual.equals("primer pis") && plantaNova.equals ("baixar un")) {
             System.out.println("planta baixa");
+          
+       // Segon pis   
             
         } else if (plantaActual.equals("segon pis") && plantaNova.equals ("baixar un")) {
             System.out.println("primer pis");
