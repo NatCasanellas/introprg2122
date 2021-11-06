807d672d15f2ca67b104f807b378dd9878bbab7f
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-06 22:56:13 +0100

    Exercici Finalitzat

--- 01_25_nombre_de_fills/Fills.java
+++ 01_25_nombre_de_fills/Fills.java
@@ -18,7 +18,7 @@ public class Fills {
             } else if (numero == 1) {
                 System.out.println("Compte de no mimar-lo massa!");
         
-            } else if (numero >= 2) {
+            } else if (numero < 2) {
                 System.out.println("No t'avorreixes a casa, eh?");
         
             } else if (numero > 4) {
