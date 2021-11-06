2917180ab4c3d4da5f42c15b795505b662a5ba34
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-06 16:33:39 +0100

    Exercici Finalitzat

---
+++ 01_23_pagaments/Pagament.java
@@ -0,0 +1,29 @@
+/*
+* Aquest programa simula una transaccio amb la quantitat a pagar i la pagada,
+* de tal manera que el programa indica si sobren o falten diners.
+*/
+
+public class Pagament {
+    public static void main(String[] args) {
+    
+           System.out.println("Preu?");
+           int numero = Integer.parseInt(Entrada.readLine());
+    
+           System.out.println("Paga?");
+           int numero1 = Integer.parseInt(Entrada.readLine());
+    
+             if (numero < numero1)
+                System.out.println("Sobren " + numero + "€");
+        
+        
+        
+             System.out.println("Falten " + numero + "€");
+    
+    
+             if (numero == numero1) {
+    
+                System.out.println("No sobra ni falta res");
+    }
+    
+    }
+    }
