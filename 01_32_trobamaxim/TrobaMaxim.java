1b36c4f6684d8c38666f7f6d73b11092edf05582
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-09 22:36:49 +0100

    Exercici Finalitzat

--- 01_32_trobamaxim/TrobaMaxim.java
+++ 01_32_trobamaxim/TrobaMaxim.java
@@ -1,11 +1,23 @@
 /*
-*
+* El programa demana enters positius i en el moment que rep un de negatiu, 
+* mostra el valor maxim positiu intriduït i finalitza.
 */
 
 public class TrobaMaxim {
     public static void main(String[] args) {
     
-    
+          int suma = 0;
+          int valor = 0;
+          
+          while (valor >= 0) {
+               System.out.println("Introdueix un valor");
+               valor = Integer.parseInt(Entrada.readLine());
+               if (valor >= 0) {
+               System.out.println("Introdueix un valor");
+                    suma = suma + valor;
+               }
+            }
+            System.out.println("Valors parells introduïts: " + suma);
     
     }
 
