4e14794fa44ca51cd5591c40b1bc61284e32efb2
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-09 22:47:36 +0100

    Exercici Finalitzat

--- 01_32_trobamaxim/TrobaMaxim.java
+++ 01_32_trobamaxim/TrobaMaxim.java
@@ -12,7 +12,7 @@ public class TrobaMaxim {
           while (valor >= 0) {
                System.out.println("Introdueix un valor");
                valor = Integer.parseInt(Entrada.readLine());
-               if (valor <= 0) {
+               while (valor >= 0) {
                System.out.println("Introdueix un valor");
                     suma = suma + valor;
                }
