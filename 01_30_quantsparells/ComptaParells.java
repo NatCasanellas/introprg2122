b390f5413dad09a5bcbbe23fdff752537661f7d7
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-09 12:58:21 +0100

    Exercici Finalitzat

--- 01_30_quantsparells/ComptaParells.java
+++ 01_30_quantsparells/ComptaParells.java
@@ -10,11 +10,11 @@ public class ComptaParells {
           int suma = 0;
           int valor = 0;
           
-          while (valor >= 0) {
+          while ((valor >= 0 && valor <= 0)) {
                System.out.println("Introdueix un valor");
                valor = Integer.parseInt(Entrada.readLine());
-               if (valor <= 0) {
-                    suma = suma - valor;
+               if (valor >= 0) {
+                    suma = suma + valor;
                }
                
             }
