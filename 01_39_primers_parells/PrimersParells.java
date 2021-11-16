be09de9f2ec913a47f158112879e7aec9c35d5e5
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-16 17:18:46 +0100

    Exercici Finalitzat

---
+++ 01_39_primers_parells/PrimersParells.java
@@ -0,0 +1,18 @@
+/*
+* Elprograma escriu els primers numeros naturals que siguin parells fins el 10.
+*/
+
+public class PrimersParells {
+    public static void main(String[] args) {
+    
+         int numero = 2;
+         
+         while ((numero%1) == 0) {
+            System.out.println(numero);
+            
+            
+         }
+    
+    }
+
+}
