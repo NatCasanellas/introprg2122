b1dadbc847e101a2e217ea4678dcce866a57483a
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-16 16:57:58 +0100

    Exercici Finalitzat

--- 01_38_deu_primers_decreixent/DeuPrimersDecreixent.java
+++ 01_38_deu_primers_decreixent/DeuPrimersDecreixent.java
@@ -5,11 +5,11 @@
 public class DeuPrimersDecreixent {
     public static void main(String[] args) {
     
-         int numero = 1;         // variable de recorregut assignada al primer valor
+         int numero = 10;        // variable de recorregut assignada al primer valor
          
-         while (numero <= 10) {  // condició de sortida en passar del darrer valor
+         while (numero >= 1) {  // condició de sortida en passar del darrer valor
              System.out.println(numero);
-             numero = numero + 1;    // passem el número al següent
+             numero = numero - 1;    // passem el número al següent
          }
     
     }
