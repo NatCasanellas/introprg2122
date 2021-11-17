e0404818528f8cfaab28b22d2f2def6c78e09fc5
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-17 18:04:46 +0100

    Exercici Finalitzat

---
+++ 01_40_de_principi_a_fi/DeuPrimers.java
@@ -0,0 +1,24 @@
+/*
+*
+*/
+
+public class DeuPrimers {
+    public static void main(String[] args) {
+    
+    for (int numero = 1;             // variable de recorregut
+         numero <= 10;               // condició
+         numero = numero + 1) {      // passa al número següent
+         System.out.println(numero); // processa el valor
+     
+     
+     //int numero = 1;         // variable de recorregut assignada al primer valor
+         
+      //   while (numero <= 10) {  // condició de sortida en passar del darrer valor
+       //      System.out.println(numero);
+        //     numero = numero + 1; 
+     
+      }
+    
+    }
+
+}
