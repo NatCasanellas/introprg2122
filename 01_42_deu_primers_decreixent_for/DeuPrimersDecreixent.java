1498ee21999127da84ce76496a79ce39044aba39
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-17 18:28:44 +0100

    Exercici Finalitzat

---
+++ 01_42_deu_primers_decreixent_for/DeuPrimersDecreixent.java
@@ -0,0 +1,16 @@
+/*
+* Aques programa escriu els primers 10 numeros naturals començant pel mes gran
+*/
+
+public class DeuPrimersDecreixent {
+    public static void main(String[] args) {
+    
+        for (int numero = 10;       
+             numero >= 1;
+             numero = numero - 1) { 
+             System.out.println(numero);
+             
+         }
+    }
+
+}
