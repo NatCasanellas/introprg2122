fe5a8455d51d669e21d8ce8ac901aad68b78ca49
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-17 18:17:25 +0100

    Exercici Finalitzat

---
+++ 01_41_deu_primers_for/DeuPrimers.java
@@ -0,0 +1,16 @@
+/*
+* El programa escriu els 10 primers numeros naturals
+*/
+
+public class DeuPrimers {
+    public static void main(String[] args) {
+    
+        for (int numero = 1;
+             numero <= 10;
+             numero = numero + 1) {
+             System.out.println(numero);
+        }
+    
+    }
+
+}
