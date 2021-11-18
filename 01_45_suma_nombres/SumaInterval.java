029f1f789d4fb36c7dfd7261c9a45a1be82ab753
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-18 10:22:31 +0100

    Exercici Finalitzat

---
+++ 01_45_suma_nombres/SumaInterval.java
@@ -0,0 +1,18 @@
+/*
+* El programa demanarà dos valors enters i sumarà tots els nombres que hi 
+* hagin entre el més petit i el més gran d'ells. 
+*/ 
+
+public class SumaInterval {
+    public static void main(String[] args) {
+    
+        for (int inici = Integer.parseInt(Entrada.readLine());
+             System.out.println("inici?");
+             int fi = Integer.parseInt(Entrada.readLine());
+             System.out.println("final?");
+             inici <= fi;
+             
+    
+    }
+
+}
---
+++ 01_46_mostra_interval/MostraInterval.java
@@ -0,0 +1,29 @@
+/*
+* El programa demana dos valors enters i mostra tots els enters que hi ha
+* entre el primer i el segon, en l'ordre marcat per l'entrada.
+*/
+
+public class MostraInterval {
+    public static void main(String[] args) {
+    
+            int valor = Integer.parseInt(Entrada.readLine());
+            int fi = Integer.parseInt(Entrada.readLine());
+    
+    
+        for (valor = Integer.parseInt(Entrada.readLine());   //inici;
+             valor <= fi;
+             valor = valor + 1) {
+             System.out.println(valor);
+             
+             }
+    
+    
+        for (valor = Integer.parseInt(Entrada.readLine());    //inici;
+             valor >= fi;
+             valor = valor -1) {
+             System.out.println(valor);
+             
+             }
+    }
+
+}
