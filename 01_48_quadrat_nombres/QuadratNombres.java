7cc65eaa9c770d415b2ca40a0c599cf6bea76989
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-20 17:06:31 +0100

    Exercici Finalitzat

--- 01_48_quadrat_nombres/QuadratNombres.java
+++ 01_48_quadrat_nombres/QuadratNombres.java
@@ -5,12 +5,18 @@
 public class QuadratNombres {
     public static void main(String[] args) {
     
+    
         for (int linia=1; linia <= 5; linia++) {
-        for (int columna=1; columna <= 5; columna++) {
-             System.out.print(" " + columna);
+            System.out.println(" 1 2 3 4 5");
         }
-             System.out.println();
-        }
+    
+    
+       // for (int linia=1; linia <= 5; linia++) {
+       // for (int columna=1; columna <= 5; columna++) {
+      //       System.out.print(" " + columna);
+      //  }
+      //       System.out.println();
+        //}
     
     }
 
