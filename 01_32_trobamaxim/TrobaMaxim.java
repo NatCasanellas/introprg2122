899579d0508a550bc71c7a42f74bcaf531f1a358
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-23 17:41:17 +0100

    Exercici Finalitzat

--- 01_32_trobamaxim/TrobaMaxim.java
+++ 01_32_trobamaxim/TrobaMaxim.java
@@ -6,20 +6,24 @@
 public class TrobaMaxim {
     public static void main(String[] args) {
     
-          int maxim = 0;
+          System.out.println("Introdueix un valor");
+          int maxim = Integer.parseInt(Entrada.readLine());
           int valor = 0;
           
     //mentres el numero sigui >= 0 seguira preguntant al usuari el numero
           while (maxim >= 0) {
-          System.out.println("Introdueix un valor");
-          maxim = Integer.parseInt(Entrada.readLine());
+          //System.out.println("Introdueix un valor");
+          //maxim = Integer.parseInt(Entrada.readLine());
           
    //quan aquest numero sigui negatiu, anira suman en 1 el numero total de  parells 
-          if (maxim <= 0 && maxim >= 0) {
-                  valor = valor - 1;
+          if (valor <= 0) {
+                  //valor = valor - 1;
             }
           }
  //quan el numero sigui negatiu, sortira del bucle i mostrara el maxim valor positiu       
           System.out.println("El màxim és " + valor);
     }
 }
+
+
+
