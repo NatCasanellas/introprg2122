4b5930dd07d3190210ceb96a6f33a9667033119e
NatCasanellas <cf21natalia.casanellas@iesjoandaustria.org>
2021-11-23 10:34:23 +0100

    Exercici Finalitzat

--- 01_51_triangles/Triangle.java
+++ 01_51_triangles/Triangle.java
@@ -16,7 +16,7 @@ public class Triangle {
        //Declarem per quin numero comença y i acaba 9 al 0, afegim ..... linia 
             for (int lin = 9; lin >= 0; lin--) {
                 for (int puntoI = 0; puntoI < lin; puntoI++){
-                     System.out.println("9");
+                     System.out.println(".");
                 }
             //            
                 for (int numerosI = lin; numerosI < 10; numerosI++) {
@@ -28,7 +28,7 @@ public class Triangle {
             }
             //Declarem per quin numero comença i acaba 9 al 0, afegim ....linia
             for (int puntoI = 0; puntoI < lin; puntoI++) {
-                System.out.println("9");
+                System.out.println(".");
             }
         System.out.println();
       } 
